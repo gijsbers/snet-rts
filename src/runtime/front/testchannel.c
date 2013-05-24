d5eb1426e6a9d70c8f27cd9ce32867fbc41dfb9e
Bert Gijsbers <gijsbers@science.uva.nl>
2013-05-24 18:04:31 +0200

    Test ChannelMerge add wild.

--- src/runtime/front/testchannel.c
+++ src/runtime/front/testchannel.c
@@ -1,6 +1,7 @@
 #include "xchannel.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
@@ -11,6 +12,174 @@ void *SNetMemAlign(size_t s) { return malloc(s); }
 void SNetMemFree(void *p) { return free(p); }
 
 static int verb;
+
+  typedef struct state {
+    int get;
+    int put;
+    channel_t *chan;
+    struct state *next, *prev;
+  } state_t;
+
+static void check(state_t *st)
+{
+  CHAN_CHECK(st->chan);
+}
+
+static state_t *new_state(void)
+{
+  state_t *st = SNetMemAlloc(sizeof(state_t));
+  st->get = rand() & 4095;
+  st->put = st->get;
+  st->chan = SNetChannelCreate();
+  st->next = NULL;
+  st->prev = NULL;
+  return st;
+}
+
+static void del_state(state_t *st)
+{
+  free(st);
+}
+
+static double prob(void)
+{
+  return (double) rand() / (double) RAND_MAX;
+}
+
+static void append(state_t *st)
+{
+  long *n;
+  state_t *next = st;
+  check(st);
+  while (next->next) {
+    next = next->next;
+  }
+  n = SNetNew(long);
+  *n = next->put++;
+  SNetChannelPut(st->chan, n);
+}
+
+static state_t *get_last(state_t *st)
+{
+  state_t *next = st;
+  while (next->next) {
+    next = next->next;
+  }
+  return next;
+}
+
+static bool reduce(state_t *st)
+{
+  check(st);
+  while (st->get == st->put && st->next) {
+    state_t *next = st->next;
+    st->get = next->get;
+    st->put = next->put;
+    st->next = next->next;
+    del_state(st);
+  }
+  if (st->get < st->put) {
+    long *n = SNetChannelGet(st->chan);
+    assert(*n == st->get);
+    st->get++;
+    free(n);
+    return true;
+  }
+  return false;
+}
+
+static bool empty(state_t *st)
+{
+  return st->get == st->put && !st->next && !st->next;
+}
+
+static void merge(state_t *st, state_t *next)
+{
+  state_t *last;
+  check(st);
+  check(next);
+  SNetChannelMerge(st->chan, next->chan);
+  next->chan = NULL;
+  last = get_last(st);
+  last->next = next;
+  next->prev = last;
+}
+
+static void test_wild(void)
+{
+  const int num = 128;
+  state_t *arr[num];
+  int i, j, k, nrec = 0, nst = 0, nmrg = 0;
+  const int max_k = 10;
+
+  for (i = 0; i < num; ++i) {
+    arr[i] = NULL;
+  }
+
+  for (k = 0;; ++k) {
+    if (k < max_k && prob() < 0.05) {
+      for (i = 0; i < num; ++i) {
+        if (arr[i] == NULL) {
+          arr[i] = new_state();
+          ++nst;
+        }
+      }
+    }
+    else if (k < max_k && prob() < 0.60) {
+      for (i = rand() % num; i < num; ++i) {
+        if (arr[i]) {
+          append(arr[i]);
+          ++nrec;
+          break;
+        }
+      }
+    }
+    else if (prob() < 0.03) {
+      for (i = rand() % num; i < num; ++i) {
+        if (arr[i]) {
+          for (j = rand() % num; j < num; ++j) {
+            if (j != i && arr[j]) {
+              merge(arr[i], arr[j]);
+              arr[j] = NULL;
+              --nst;
+              ++nmrg;
+              break;
+            }
+          }
+          break;
+        }
+      }
+    }
+    else {
+      for (i = rand() % num; i < num; ++i) {
+        if (arr[i]) {
+          if (reduce(arr[i])) {
+            --nrec;
+          }
+          else {
+            assert(empty(arr[i]));
+          }
+          if (empty(arr[i])) {
+            del_state(arr[i]);
+            arr[i] = NULL;
+            --nst;
+          }
+          break;
+        }
+      }
+    }
+    if (!nst && !nrec && k >= max_k) {
+      printf("done at k = %d\n", k);
+      break;
+    }
+    if (k % 100 == 0) {
+      printf("st %d, rec %d, merg %d\n", nst, nrec, nmrg);
+    }
+  }
+  for (i = 0; i < num; ++i) {
+    assert(arr[i] == NULL);
+  }
+}
 
 static void test_merge(void)
 {
@@ -144,14 +313,16 @@ static void test_single(void)
 int main(int argc, char **argv)
 {
   int c;
-  int merg = 1, sing = 1;
+  int merg = 0, sing = 0, wild = 1;
   unsigned seed = 0;
 
-  while ((c = getopt(argc, argv, "vms:")) != EOF) {
+  while ((c = getopt(argc, argv, "ims:vw")) != EOF) {
     switch (c) {
       case 'v': verb = 1; break;
       case 's': sscanf(optarg, "%u", &seed); break;
-      case 'm': merg = 1; sing = 0; break;
+      case 'i': sing = 1; break;
+      case 'm': merg = 1; break;
+      case 'w': wild = 1; break;
       default: printf("%s: bad option -%c\n", *argv, c); exit(1);
     }
   }
@@ -161,6 +332,9 @@ int main(int argc, char **argv)
   }
   srand(seed);
 
+  if (wild) {
+    test_wild();
+  }
   if (merg) {
     test_merge();
   }
